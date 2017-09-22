4592821845e6246b1e5492261f9002fc65fffe2a
mankityau <david.mankit.yau@gmail.com>
2017-09-22 00:15:54 -0700

    Cosmetic.

--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -4,36 +4,36 @@
 
 // partitions elements low through high (inclusive)
 // around a pivot and returns the pivot index
-int partition(std::vector<int>& data, int low, int high) {
-  // your code here
+int partition(std::vector<int> &data, int low, int high) {
+    // your code here
 }
 
 // sorts elements low through high (inclusive) using a single thread
-void quicksort(std::vector<int>& data, int low, int high) {
-  // your code here
+void quicksort(std::vector<int> &data, int low, int high) {
+    // your code here
 }
 
 // sorts elements low through high (inclusive) using multiple threads
-void parallel_quicksort(std::vector<int>& data, int low, int high) {
-  // your code here
+void parallel_quicksort(std::vector<int> &data, int low, int high) {
+    // your code here
 }
 
 int main() {
 
-  // create two copies of random data
-  const int VECTOR_SIZE = 1000000;
-  std::vector<int> v1(VECTOR_SIZE, 0);
-  // fill with random integers
-  for (int i=0; i<VECTOR_SIZE; ++i) {
-    v1[i] = rand();
-  }
-  std::vector<int> v2 = v1;  // copy all contents
+    // create two copies of random data
+    const int VECTOR_SIZE = 1000000;
+    std::vector<int> v1(VECTOR_SIZE, 0);
+    // fill with random integers
+    for (int i = 0; i < VECTOR_SIZE; ++i) {
+        v1[i] = rand();
+    }
+    std::vector<int> v2 = v1;  // copy all contents
 
-  // sort v1 using sequential algorithm
-  quicksort(v1, 0, v1.size()-1);
+    // sort v1 using sequential algorithm
+    quicksort(v1, 0, v1.size() - 1);
 
-  // sort v2 using parallel algorithm
-  parallel_quicksort(v2, 0, v2.size()-1);
+    // sort v2 using parallel algorithm
+    parallel_quicksort(v2, 0, v2.size() - 1);
 
-  return 0;
+    return 0;
 }
