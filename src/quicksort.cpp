1dc0bdda6341c8425d442458331708d41dc389ad
mankityau <david.mankit.yau@gmail.com>
2017-09-22 03:19:45 -0700

    Introduced unit test for quicksort.

--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -2,15 +2,54 @@
 #include <thread>
 #include <vector>
 
+/**
+ * @brief       Swap two element in the vector
+ * @param data  The vector that will have its element swapped
+ * @param a
+ * @param b
+ */
+void swap(std::vector<int> &data, int a, int b) {
+    int temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+}
+
 // partitions elements low through high (inclusive)
 // around a pivot and returns the pivot index
+/**
+ *
+ * @param data  The entire vector that the function is going
+ * @param low
+ * @param high
+ * @return  the pivot index that function used.
+ */
 int partition(std::vector<int> &data, int low, int high) {
-    // your code here
+    int pivotValue = data[high];
+    int swapIndex = low;
+    for (int i = low; i < high; i++) {
+        if (data[i] < pivotValue) {
+            swap(data, i, swapIndex);
+            swapIndex ++;
+        }
+    }
+
+    swap(data, swapIndex, high);
+    return swapIndex;
 }
 
 // sorts elements low through high (inclusive) using a single thread
+/**
+ * @brief       The function will sort the specific range of the vector. Range = [lower bound, higher bound]
+ * @param data  The vector that will be sorted
+ * @param low   The lower bound
+ * @param high  The higher bound
+ */
 void quicksort(std::vector<int> &data, int low, int high) {
-    // your code here
+    if (low < high) {
+        int pivotIndex = partition(data, low, high);
+        quicksort(data, low, pivotIndex - 1);
+        quicksort(data, pivotIndex + 1, high);
+    }
 }
 
 // sorts elements low through high (inclusive) using multiple threads
@@ -18,22 +57,32 @@ void parallel_quicksort(std::vector<int> &data, int low, int high) {
     // your code here
 }
 
-int main() {
-
-    // create two copies of random data
-    const int VECTOR_SIZE = 1000000;
-    std::vector<int> v1(VECTOR_SIZE, 0);
-    // fill with random integers
-    for (int i = 0; i < VECTOR_SIZE; ++i) {
-        v1[i] = rand();
-    }
-    std::vector<int> v2 = v1;  // copy all contents
-
-    // sort v1 using sequential algorithm
-    quicksort(v1, 0, v1.size() - 1);
-
-    // sort v2 using parallel algorithm
-    parallel_quicksort(v2, 0, v2.size() - 1);
-
-    return 0;
-}
+//int main() {
+//
+//    // create two copies of random data
+//    const int VECTOR_SIZE = 10;
+//    std::vector<int> v1(VECTOR_SIZE, 0);
+//    // fill with random integers
+//    for (int i = 0; i < VECTOR_SIZE; ++i) {
+//        v1[i] = rand();
+//    }
+////    std::vector<int> v2 = v1;  // copy all contents
+//
+//    // sort v1 using sequential algorithm
+//    for (int i = 0; i < VECTOR_SIZE; ++i) {
+//        std::cout << v1[i] << "    ";
+//    }
+//
+//    std::cout << "\n";
+//
+//    quicksort(v1, 0, v1.size() - 1);
+//    for (int i = 0; i < VECTOR_SIZE; ++i) {
+//        std::cout << v1[i] << "    ";
+//    }
+//
+//
+//    // sort v2 using parallel algorithm
+////    parallel_quicksort(v2, 0, v2.size() - 1);
+//
+//    return 0;
+//}
---
+++ test/quicksortUnitTest.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+#include "quicksort.cpp"
+
+TEST(quicksortUnitTest, quicksort_emptyVector){
+    std::vector<int> unsortedVector = {};
+    std::vector<int> expectedVector = {};
+    quicksort(unsortedVector, 0, 0);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_sizeOneVector) {
+    std::vector<int> unsortedVector = {0};
+    std::vector<int> expectedVector = {0};
+    quicksort(unsortedVector, 0, 0);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_unsortedVectorOddLength) {
+    std::vector<int> unsortedVector = {4, 2, 3, 1, 0};
+    std::vector<int> expectedVector = {0, 1, 2, 3, 4};
+    quicksort(unsortedVector, 0, 4);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_unsortedVectorEvenLength) {
+    std::vector<int> unsortedVector = {4, 2, 5, 3, 1, 0};
+    std::vector<int> expectedVector = {0, 1, 2, 3, 4, 5};
+    quicksort(unsortedVector, 0, 5);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_sortedVector) {
+    std::vector<int> unsortedVector = {0, 1, 2, 3, 4, 5};
+    std::vector<int> expectedVector = {0, 1, 2, 3, 4, 5};
+    quicksort(unsortedVector, 0, 5);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_flippedVector) {
+    std::vector<int> unsortedVector = {5, 4, 3, 2, 1, 0};
+    std::vector<int> expectedVector = {0, 1, 2, 3, 4, 5};
+    quicksort(unsortedVector, 0, 5);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_duplicatedElementVector) {
+    std::vector<int> unsortedVector = {5, 4, 2, 2, 4, 0};
+    std::vector<int> expectedVector = {0, 2, 2, 4, 4, 5};
+    quicksort(unsortedVector, 0, 5);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
+TEST(quicksortUnitTest, quicksort_sameElementVector) {
+    std::vector<int> unsortedVector = {5, 5, 5, 5, 5, 5};
+    std::vector<int> expectedVector = {5, 5, 5, 5, 5, 5};
+    quicksort(unsortedVector, 0, 5);
+    EXPECT_EQ(unsortedVector, expectedVector);
+}
+
