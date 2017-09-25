a0732f11f10b53663506bc83cf014ad8afe3255e
Yau Man Kit <Yau Man Kit>
2017-09-25 14:36:58 -0700

    Finished Q2 Part 1.

---
+++ src/pi.h
@@ -0,0 +1,12 @@
+#ifndef LAB2_PI_H
+#define LAB2_PI_H
+
+namespace pi {
+    /**
+     * estimate the value of pi using n-number of samples.
+     * @param nsamples the number of samples
+     * @return the esimated value of pi
+     */
+    double estimate_pi(int nsamples);
+}
+#endif //LAB2_PI_H
---
+++ src/piMain.cpp
@@ -0,0 +1,7 @@
+#include "pi.h"
+
+int main(){
+    double pi = pi::estimate_pi(1000);
+    std::cout << "My estimate of PI is: " << pi << std::endl;
+    return 0;
+}
